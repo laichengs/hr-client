ba31d194fa25a0fa89632362dfb329bd14ee063f
unknown <241164@qq.com>
2019-03-11 14:22:58 +0800

    更换域名

--- config/index.js
+++ config/index.js
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://minicdn.meijinguanjia.com/',
+    assetsPublicPath: 'https://cdn.lail.cn/',
 
     /**
      * Source Maps
